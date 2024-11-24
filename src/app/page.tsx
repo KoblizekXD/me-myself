cd4533586e16456d1cc1d33d40a5be2b3a0a3539
KoblizekXD <janprokupek@centrum.cz>
2024-11-24 08:43:55 +0100

    Finish the main window

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,54 @@
 'use client';
 
 import { Window } from "@/components/window";
-import { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  const [isMaximized, setIsMaximized] = useState(false);
-
   return (
     <main className="bg-[#1E1E2E] flex flex-col justify-center items-center w-screen h-screen">
-      <Window />
+      <Window className="flex" title="aa55h - @koblizekxd">
+        <div className="flex flex-col gap-y-2 basis-1/3 justify-center items-center">
+          <Image width={128} height={128} className="rounded-xl" src={'https://github.com/KoblizekXD.png'} alt="Not found!" />
+          <h1 className={'text-xl font-bold text-yellow-400'}>Hey! That's me!</h1>
+          <h2 className={'font-semibold text-blue-400'}>@aa55h</h2>
+        </div>
+        <div className="flex-1 flex flex-col gap-y-1 py-2 px-4">
+          <h1 className={'font-bold text-2xl text-green-500'}># About me</h1>
+          <p>
+            Hi! I'm aa55h, a young developer from Czech Republic. Great to see you around!
+            This is my personal website where I share my projects and other stuff. Take 
+            a look around if you want to know more about me!
+          </p>
+          <h1 className={'font-bold text-2xl text-green-500'}># Notable Projects</h1>
+          <p>
+            Throughout my coding journey, I've worked on quite a few projects. Here are some of them:
+          </p>
+          <ul className="font-semibold gap-y-1 flex flex-col">
+            <li className="flex">
+              <Link className="underline" href={'https://www.github.com/KoblizekXD/byte-lens'}>
+                ByteLens
+              </Link>
+              &nbsp;- Simple Java bytecode viewer written in JavaFX.
+            </li>
+            <li>
+              <Link className="underline" href={'https://www.github.com/Chigga-Solutions/Cock-Down-Shooter'}>
+                Cock Down Shooter
+              </Link>
+              &nbsp;- Online game about shooting down chicken!
+            </li>
+            <li>
+              <Link className="underline" href={'https://github.com/KoblizekXD/myedu'}>
+                MyEdu
+              </Link>
+              &nbsp;- Simple grading software
+            </li>
+          </ul>
+          <p className="text-violet-300 font-extrabold">
+            See the projects window for more!
+          </p>
+        </div>
+      </Window>
     </main>
   );
 }
--- src/components/window.tsx
+++ src/components/window.tsx
@@ -1,8 +1,14 @@
 'use client';
 
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
-export function Window() {
+interface WindowProps {
+  title?: string;
+  children?: React.ReactNode;
+  className?: string;
+}
+
+export function Window({ title = "Window", children, className }: WindowProps) {
   const [isMaximized, setIsMaximized] = useState(false);
   const [isDragging, setIsDragging] = useState(false);
   const windowRef = useRef<HTMLDivElement>(null);
@@ -10,19 +16,29 @@ export function Window() {
   useEffect(() => {
     const handleMouseMove = (e: MouseEvent) => {
       if (isDragging && windowRef.current) {
-        windowRef.current.style.left = `${e.clientX}px`;
-        windowRef.current.style.top = `${e.clientY}px`;
+        windowRef.current.style.left = `${e.clientX + (windowRef.current.clientWidth / 2) - 10}px`;
+        windowRef.current.style.top = `${e.clientY + (windowRef.current.clientHeight / 2) - 10}px`;
       }
     };
 
+    const handleKeyPress = (e: KeyboardEvent) => {
+      console.log(e.key);
+      
+      if (e.key === 'Escape') {
+        setIsDragging(false);
+      }
+    }
+
     document.addEventListener('mousemove', handleMouseMove);
+    document.addEventListener('keydown', handleKeyPress);
     return () => {
       document.removeEventListener('mousemove', handleMouseMove);
+      document.removeEventListener('keydown', handleKeyPress);
     }
   }, [isDragging]);
 
   return (
-    <div onMouseUp={() => setIsDragging(false)} ref={windowRef} className={`rounded-xl transition-transform absolute ${isMaximized ? 'w-full h-full left-0 top-0' : 'w-1/2 h-1/2 left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2'} shadow bg-[#181825]`}>
+    <div onMouseUp={() => setIsDragging(false)} ref={windowRef} className={`rounded-xl flex flex-col transition-transform absolute ${isMaximized ? 'w-full h-full left-0 top-0' : 'w-1/2 h-1/2 left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2'} shadow bg-[#181825]`}>
       <div onMouseDown={() => {
         setIsMaximized(false);
         setIsDragging(true);
@@ -34,7 +50,10 @@ export function Window() {
           }} title="Maximize" className={'rounded-[50%] cursor-pointer w-[15px] h-[15px] inline-block bg-[#FFBC2C]'}></span>
           <span title="Minimize" className={'rounded-[50%] cursor-pointer w-[15px] h-[15px] inline-block bg-[#28C940]'}></span>
         </div>
-        <h1 className="text-[#FFBC2C] select-none pointer-events-none absolute left-1/2 -translate-x-1/2 text-sm font-bold">aa55h - @koblizekxd</h1>
+        <h1 className="text-[#FFBC2C] select-none pointer-events-none absolute left-1/2 -translate-x-1/2 text-sm font-bold">{title}</h1>
+      </div>
+      <div className={`flex-1 ${className}`}>
+        {children}
       </div>
     </div>
   );
